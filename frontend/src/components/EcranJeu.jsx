9e28e5938915f4779a7d5ad17de7c8c4199ccc7a
Abdoulaye SARRE <abdoulaye.sarre0411@gmail.com>
2024-04-28 08:25:07 +0000

    front presque terminer

--- frontend/src/components/EcranJeu.jsx
+++ frontend/src/components/EcranJeu.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import Paysage from './Paysage';
+import React, { useEffect, useState } from "react";
+import Paysage from "./Paysage";
 
-function EcranJeu() {
-  return (
-    <div className='border border-gray-500 rounded w-full min-h-128 overflow-hidden'> {/* Assurez-vous que le div parent a aussi overflow-hidden */}
-      <Paysage/>
-    </div>
-  );
+function EcranJeu({ start, reponse1, reponse2 }) {
+    const [positionUn, setPositionUn] = useState(0);
+    const [positionDeux, setPositionDeux] = useState(0);
+    start = true;
+    useEffect(() => {
+        if (start) {
+            const interval = setInterval(() => {
+                setPositionUn((prevPosition) => prevPosition + 1);
+                setPositionDeux((prevPosition) => prevPosition + 1);
+            }, 50);
+            return () => clearInterval(interval);
+        }
+    }, [start]);
+    return (
+        <div className="border border-gray-500 rounded w-full max-h-128 overflow-hidden flex flex-col  ">
+            <Paysage start={start} />
+            <div className="w-full bg-gris min- h-36 py-4  flex flex-col justify-center relative">
+                <div
+                    style={{ transform: `translateX(${positionUn}px)` }}
+                    className={`" absolute -top-8 "`}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/images/car1.png"}
+                        alt="car1"
+                        className=" w-44"
+                    />
+                </div>
+                <div className="p-0.5 w-full bg-white"></div>
+                <div
+                    style={{ transform: `translateX(${positionDeux}px)` }}
+                    className={` absolute bottom-4 right-[${positionDeux}]`}
+                >
+                    <img
+                        src={process.env.PUBLIC_URL + "/images/car2.png"}
+                        alt="car1"
+                        className=" w-44"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default EcranJeu;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        rouge: '#ff6b6b',
-        jaune: '#ffcf57',
-        bleu: '#5cc0eb',
-        vert: '#48dbb4',
-        gris: '#5a5c5e',
-      },
-      fontFamily: {
-        'mont': ['Montserrat', 'sans-serif'],
-      },
-      minHeight: {
-        '128': '32rem',
-      }
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                rouge: "#ff6b6b",
+                jaune: "#ffcf57",
+                bleu: "#5cc0eb",
+                vert: "#48dbb4",
+                gris: "#5a5c5e",
+            },
+            fontFamily: {
+                mont: ["Montserrat", "sans-serif"],
+            },
+            minHeight: {
+                128: "25rem",
+            },
+        },
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
